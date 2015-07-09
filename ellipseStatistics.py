b4432eb78ed9b3bf970afc33372bb43265e7ab7e
Rachel Morrison <rlm152@case.edu>
2015-07-09 17:23:34 -0400

    attribute method works with string input with location and dates
    Signed-off-by: Rachel Morrison <rlm152@case.edu>

--- ellipseStatistics.py
+++ ellipseStatistics.py
@@ -18,33 +18,35 @@ def read_csv_file(filename):
 def slice_frame_by_attribute(frame, att):
     """
     Slices the data frame by date and returns a starting and ending indicies for subframe 
+    
+    usage: bounds = slice_fram_by_attribute(frame, attributeAsAString)
     """
    #print(frame)
     startNotSet = True
-    counter = 0
+    counter = 1
     startIndex = 0
     endIndex = 0
-    #typecast date from datetime object to string so it can be compared to d
-    att = str(att)
-    print(att)
-    
-    #
+
     for d in frame:
         if startNotSet and d == att:
+
             startIndex = counter
-            print(startIndex + " yay")
             startNotSet = False
+            
+        #print(startNotSet)
         if startNotSet == False and d != att:
-            endIndex = counter
+            endIndex = counter - 1
             break
         counter = counter + 1  
     if endIndex == 0:
-        endIndex = counter
+        endIndex = counter - 1
     return [startIndex, endIndex]
     
 def main():
     dendriteData = read_csv_file("BMGMC_dendrite_data_with_locations.csv")
-    bounds = slice_frame_by_attribute(dendriteData.iloc[1:, 2], datetime.date(2015, 7, 7))
+
+    bounds = slice_frame_by_attribute(dendriteData.iloc[1:, 1], "4")
     print(bounds)
+    
     #print(dendriteData.iloc[bounds[0]:bounds[1], 2])
-   # print(dendriteData.iloc[0:, 0:2])
+    #print(dendriteData.iloc[0:, 0:2])
