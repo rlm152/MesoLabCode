c510613a06ee123d9a4d8bc695e51178b3eaa52c
Rachel Morrison <rlm152@case.edu>
2015-07-14 15:49:59 -0400

    aspect ratio function works
    Signed-off-by: Rachel Morrison <rlm152@case.edu>

--- ellipseStatistics.py
+++ ellipseStatistics.py
@@ -6,7 +6,6 @@ Created on Wed Jul 08 13:54:45 2015
 """
 
 import pandas 
-import numpy as np
 
 def read_csv_file(filename):
     """
@@ -30,43 +29,42 @@ def slice_frame_by_attribute(frame, att):
     startIndex = 0
     endIndex = 0
 
-    #
+    #cycles through the items in the frame and finds the start and end of a group of an attribute
     for d in frame:
         if startNotSet and d == att:
-
             startIndex = counter
             startNotSet = False
-            
-        #print(startNotSet)
         if startNotSet == False and d != att:
-            endIndex = counter - 1
+            endIndex = counter
             break
         counter = counter + 1  
     if endIndex == 0:
-        endIndex = counter - 1
+        endIndex = counter
     return [startIndex, endIndex]
     
-def calculateAspectRatio(longAxisArray, shortAxisArray):
+def calculate_aspect_ratio(longAxisArray, shortAxisArray):
     """
-    Returns a list of aspect ratio values.
+    Returns a data frame of aspect ratio values.
     
-    usage: ratios = calculateAspectRatio(longAxisArray, shortAxisArray)
+    usage: ratios = calculate_aspect_ratio(longAxisArray, shortAxisArray)
     """
-    if(len(longAxisArray) != len(shortAxisArray)):
-        print("ERROR: invalid inputs. Arrays must be equal length")
-        return
+    #if(longAxisArray.size != shortAxisArray.size):
+        #print("ERROR: invalid inputs. Arrays must be equal length")
+        #return
     ratios = []
+    counter = 0
     for i in longAxisArray:
-        ratios.append(longAxisArray[i] / shortAxisArray[i])
-    return ratios
-    
+        ratios.append(float(longAxisArray.iloc[counter]) / float(shortAxisArray.iloc[counter]))
+        counter = counter + 1
+    return pandas.DataFrame(ratios)
+
+def plotHistogramForRegion():
+    return 0
 
 def main():
     dendriteData = read_csv_file("BMGMC_dendrite_data_with_locations.csv")
-
     bounds = slice_frame_by_attribute(dendriteData.iloc[1:, 1], "4")
-    print(bounds)
-    
-    
-    #print(dendriteData.iloc[bounds[0]:bounds[1], 2])
-    #print(dendriteData.iloc[0:, 0:2])
+    aR4 = calculate_aspect_ratio(dendriteData.iloc[bounds[0]:bounds[1], 5], dendriteData.iloc[bounds[0]:bounds[1], 6])
+    pandas.DataFrame.hist(aR4, column=None, by=None, grid=True, xlabelsize=None, xrot=None, ylabelsize=None, yrot=None, ax=None, sharex=False, sharey=False, figsize=None, layout=None, bins=5)
+    aR4.plot(kind = "hist", stacked = True, bins = 5)
+
