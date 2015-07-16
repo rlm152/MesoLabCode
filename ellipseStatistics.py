236344dc1ee83abe3050bf0fce75896ddadbaedb
Rachel Morrison <rlm152@case.edu>
2015-07-16 13:19:11 -0400

    yeah...

--- ellipseStatistics.py
+++ ellipseStatistics.py
@@ -23,7 +23,6 @@ def slice_frame_by_attribute(frame, att):
     
     usage: bounds = slice_fram_by_attribute(frame, attributeAsAString)
     """
-    
     startNotSet = True
     counter = 1
     startIndex = 0
@@ -48,9 +47,6 @@ def calculate_aspect_ratio(longAxisArray, shortAxisArray):
     
     usage: ratios = calculate_aspect_ratio(longAxisArray, shortAxisArray)
     """
-    #if(longAxisArray.size != shortAxisArray.size):
-        #print("ERROR: invalid inputs. Arrays must be equal length")
-        #return
     ratios = []
     counter = 0
     for i in longAxisArray:
@@ -58,13 +54,35 @@ def calculate_aspect_ratio(longAxisArray, shortAxisArray):
         counter = counter + 1
     return pandas.DataFrame(ratios)
 
-def plotHistogramForRegion():
-    return 0
+def plot_histogram_for_region(data, subRegions, region):
+    """
+    Plots a histogram for a strain region specified by a list a subregions. Returns an array with the mean and standard deviation.
+    
+    usage: plot_histogram_for_region(ellipseDataFrame, listOfSubRegions, intRegionNumber)
+    """
+    aspectRatios = []
+    for i in subRegions:
+        bounds = slice_frame_by_attribute(data.iloc[1:, 1], str(i))
+        #column 5 is long axes, column 6 is short axes
+        subRatio = calculate_aspect_ratio(data.iloc[bounds[0]:bounds[1], 5], data.iloc[bounds[0]:bounds[1], 6])
+        aspectRatios.append(subRatio)
+
+    aRFrame = pandas.concat(aspectRatios)
+    ax = aRFrame.plot(kind = "hist", title = ("Dendrite Aspect Ratios for Region " + str(region)), bins = 20, xticks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], legend = False)
+    ax.set_xlabel("Aspect Ratio")
+    return (aRFrame, aRFrame.mean(), aRFrame.std())
+    
+
+    
 
 def main():
     dendriteData = read_csv_file("BMGMC_dendrite_data_with_locations.csv")
-    bounds = slice_frame_by_attribute(dendriteData.iloc[1:, 1], "4")
-    aR4 = calculate_aspect_ratio(dendriteData.iloc[bounds[0]:bounds[1], 5], dendriteData.iloc[bounds[0]:bounds[1], 6])
-    pandas.DataFrame.hist(aR4, column=None, by=None, grid=True, xlabelsize=None, xrot=None, ylabelsize=None, yrot=None, ax=None, sharex=False, sharey=False, figsize=None, layout=None, bins=5)
-    aR4.plot(kind = "hist", stacked = True, bins = 5)
-
+    frame1, mean1, std1 = plot_histogram_for_region(dendriteData, [3, 4, 5], 1)
+    frame2, mean2, std2 = plot_histogram_for_region(dendriteData, [6, 7, 8, 9, 10], 2)      
+    frame3, mean3, std3 = plot_histogram_for_region(dendriteData, [11, 12, 13, 14, 15], 3)
+    frame4, mean4, std4 = plot_histogram_for_region(dendriteData, [16, 17, 18, 19, 20], 4)
+    frame5, mean5, std5 = plot_histogram_for_region(dendriteData, [21, 22, 23, 24, 25], 5)
+    
+    stats = pandas.DataFrame([[mean1, std1], [mean2, std2], [mean3, std2], [mean4, std4], [mean5, std5]])
+    print(stats)
+    
