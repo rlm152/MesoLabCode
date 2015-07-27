79e1e3684454bee9d0b52fe494087fb514e0d06d
Rachel Morrison <rlm152@case.edu>
2015-07-27 11:35:53 -0400

    working on plot_daily_average
    Signed-off-by: Rachel Morrison <rlm152@case.edu>

--- ellipseStatistics.py
+++ ellipseStatistics.py
@@ -68,7 +68,8 @@ def plot_histogram_for_region(data, subRegions, region):
         aspectRatios.append(subRatio)
 
     aRFrame = pandas.concat(aspectRatios)
-    ax = aRFrame.plot(kind = "hist", title = ("Dendrite Aspect Ratios for Region " + str(region)), bins = 20, xticks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], legend = False)
+    ax = aRFrame.plot(kind = "hist", title = ("Dendrite Aspect Ratios for Region " + str(region)), bins = 20 
+                      ,xticks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], yticks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], legend = False)
     ax.set_xlabel("Aspect Ratio")
     return (aRFrame, aRFrame.mean(), aRFrame.std())
     
@@ -77,7 +78,7 @@ def daily_std(data, day):
     """
     Calculates the standard deviation for a given day. 
     
-    usage: dailyStandardDev = (dataFrameOfData, dayAsString)
+    usage: daily_std = (dataFrameOfData, dayAsString)
     """
     #bounds for a particular day
     dBounds = slice_frame_by_attribute(data.iloc[1:, 2], day)
@@ -89,12 +90,31 @@ def daily_std(data, day):
     dRFrame = dFrame.iloc[rBounds[0] - 1 : rBounds[1] - 1 , :]
     ratios = calculate_aspect_ratio(dRFrame.iloc[:, 5], dRFrame.iloc[:, 6])
     return ratios.std()
+
+def plot_daily_average(data):
+    """
+    Plots a graph of the average dendrite aspect ratio per day with error bars of the standard deviation.
     
+    usage: 
+    """
+    days = ["6/24/2015", "7/1/2015", "7/2/2015", "7/6/2015", "7/7/2015"]
+    means = []
+    stds = []
+    for day in days:
+        #bounds for a particular day
+        dBounds = slice_frame_by_attribute(data.iloc[1:, 2], day)
+        #frame of all data for day
+        dFrame = data.iloc[dBounds[0]:dBounds[1], :]
+        ratios = calculate_aspect_ratio(dFrame.iloc[:, 5], dFrame.iloc[:, 6])
+        means.append(ratios.mean())
+        stds.append(ratios.std())
+    means = pandas.DataFrame(means)
+    ax = means.plot()
 
 def main():
     
     dendriteData = read_csv_file("BMGMC_dendrite_data_with_locations.csv")
-    '''
+    
     frame1, mean1, std1 = plot_histogram_for_region(dendriteData, [3, 4, 5], 1)
     frame2, mean2, std2 = plot_histogram_for_region(dendriteData, [6, 7, 8, 9, 10], 2)      
     frame3, mean3, std3 = plot_histogram_for_region(dendriteData, [11, 12, 13, 14, 15], 3)
@@ -103,7 +123,7 @@ def main():
     
     stats = pandas.DataFrame([[mean1, std1], [mean2, std2], [mean3, std2], [mean4, std4], [mean5, std5]])
     print(stats)
-    '''
+    
     print(daily_std(dendriteData, "6/24/2015"))
     print(daily_std(dendriteData, "7/1/2015"))
     print(daily_std(dendriteData, "7/2/2015"))
