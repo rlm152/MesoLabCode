0a58695a1269b807143c4e085d28ac46757efabb
Rachel Morrison <rlm152@case.edu>
2015-11-12 11:46:29 -0500

    graphs look good, sigma still small
    Signed-off-by: Rachel Morrison <rlm152@case.edu>

--- graphGeneration.py
+++ graphGeneration.py
@@ -8,15 +8,28 @@ import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 
-def den_vs_bulk_strain(hot, room):
+
+def ave_ar_vs_strain(hot):
+    ax = hot.plot(kind = 'scatter', x = 'bulk_strain', y = 'AR', yerr = 'std', color = 'k')    
+    ax.set_xlabel('Strain', fontsize = 20)
+    ax.set_xlim([0, 1])
+    ax.set_ylabel('Average Aspect Ratio', fontsize = 20)
+    ax.set_ylim([1, 5])
+    fit = np.polyfit(hot['bulk_strain'], hot['AR'], 2)
+    x = np.linspace(0, 1, 20) 
+    y = fit[2] + (fit[1] * x) + (fit[0] * (x**2)) 
+    ax.plot(x, y, 'k--')
+    ax.legend(['raw',' y = {0}x^2 + {1}x + {2}'.format("{:.2f}".format(fit[0]), "{:.2f}".format(fit[1]), "{:.2f}".format(fit[2]))], 2, fontsize = 14)
+
+
+def den_vs_bulk_strain(hot):
     '''
     plots the dendrite vs. bulk strain given a data frame
     
     '''
+    #plt.rcParams['mathtext.default']='custom'
     
-    hot['den_min'] = (((hot['AR'] - hot['std']) / 1.324021) ** .5) -1
-    hot['den_max'] = (((hot['AR'] + hot['std']) / 1.324021) ** .5) -1
-    ax = hot.plot(kind = 'scatter',  x = 'bulk_strain', y = 'den_strain', color = 'k', label = 'data')
+    ax = hot.plot(kind = 'scatter',  x = 'bulk_strain', y = 'den_strain', color = 'k')
     ax.set_xlabel('Bulk Strain', fontsize = 20)
     ax.set_xlim([0, 1])
     ax.set_ylabel('Dendrite Strain', fontsize = 20)
@@ -30,23 +43,24 @@ def den_vs_bulk_strain(hot, room):
     minY = minFit[0] * x + minFit[1]
     regY = regFit[0] * x + regFit[1]
     maxY = maxFit[0] * x + maxFit[1]
-    #ax.plot(x, minY, 'k--',label = '1 Sigma')
     ax.plot(x, regY, 'k-')
     ax.plot(x, maxY, 'k--')
     
     #plots Jesi's strain data on the linear regression of my data
-    ax.plot([0.20, 0.31], [regFit[0] * 0.20 + regFit[1], regFit[0] * 0.31 + regFit[1]], 'w^', label = 'EBSD data')
-
+    ax.plot([0.20, 0.31], [regFit[0] * 0.20 + regFit[1], regFit[0] * 0.31 + regFit[1]], 'w^')
     
     ax.plot(hot['bulk_strain'], hot['den_max'], 'k.')
-    
-    print(ax.get_legend_handles_labels())
-    ax.legend(['reference', 'strain', '1 sigma','EBSD strain'], 2, fontsize = 14)
+    ax.legend(['reference', 'strain', '1 ' + r'${\sigma}$','EBSD strain'], 2, fontsize = 16)
     ax.plot(hot['bulk_strain'], hot['den_min'], 'k.')
-    ax.plot(x, minY, 'k--',label = '1 Sigma')
+    ax.plot(x, minY, 'k--')
     
 def main():
-    h = pandas.read_csv('bulk_den_strain_hot.csv', sep = ",", header = 0)
-    r = pandas.read_csv('bulk_den_strain_rt.csv', sep = ",", header = 0)
-    den_vs_bulk_strain(h, r)
+    hot = pandas.read_csv('bulk_den_strain_hot.csv', sep = ",", header = 0)
+    room = pandas.read_csv('bulk_den_strain_rt.csv', sep = ",", header = 0)
+    
+    hot['den_min'] = (((hot['AR'] - hot['std']) / 1.324021) ** .5) -1
+    hot['den_max'] = (((hot['AR'] + hot['std']) / 1.324021) ** .5) -1
+    
+    den_vs_bulk_strain(hot)
+    #ave_ar_vs_strain(hot)
     
