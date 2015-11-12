0e7f91933f78db44f1ec2101f2f0c206c3fdaea9
Rachel Morrison <rlm152@case.edu>
2015-11-12 08:36:56 -0500

    legend fixed
    Signed-off-by: Rachel Morrison <rlm152@case.edu>

--- graphGeneration.py
+++ graphGeneration.py
@@ -30,19 +30,20 @@ def den_vs_bulk_strain(hot, room):
     minY = minFit[0] * x + minFit[1]
     regY = regFit[0] * x + regFit[1]
     maxY = maxFit[0] * x + maxFit[1]
-    ax.plot(x, minY, 'k--',label = '1 Sigma')
+    #ax.plot(x, minY, 'k--',label = '1 Sigma')
     ax.plot(x, regY, 'k-')
     ax.plot(x, maxY, 'k--')
     
     #plots Jesi's strain data on the linear regression of my data
     ax.plot([0.20, 0.31], [regFit[0] * 0.20 + regFit[1], regFit[0] * 0.31 + regFit[1]], 'w^', label = 'EBSD data')
 
-    ax.plot(hot['bulk_strain'], hot['den_min'], 'k.')
+    
     ax.plot(hot['bulk_strain'], hot['den_max'], 'k.')
     
     print(ax.get_legend_handles_labels())
-    ax.legend(['reference', '1 sigma', 'strain'], 2, fontsize = 14)
-    
+    ax.legend(['reference', 'strain', '1 sigma','EBSD strain'], 2, fontsize = 14)
+    ax.plot(hot['bulk_strain'], hot['den_min'], 'k.')
+    ax.plot(x, minY, 'k--',label = '1 Sigma')
     
 def main():
     h = pandas.read_csv('bulk_den_strain_hot.csv', sep = ",", header = 0)
