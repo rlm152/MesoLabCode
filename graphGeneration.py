9497c8018aa2aa102aa3faa010c84313df389dc1
Rachel Morrison <rlm152@case.edu>
2015-11-23 17:52:08 -0500

    made pretty graphs with set intercepts
    Signed-off-by: Rachel Morrison <rlm152@case.edu>

--- graphGeneration.py
+++ graphGeneration.py
@@ -12,17 +12,25 @@ import scipy as sp
 import sklearn.linear_model as lm
 
 def ave_ar_vs_strain(hot):
-    ax = hot.plot(kind = 'scatter', x = 'bulk_strain', y = 'AR', yerr = 'std', color = 'k')    
+    
+    ax = hot.plot(kind = 'scatter', x = 'bulk_strain', y = 'AR', yerr = 'std', color = 'k', grid = 'off')    
     ax.set_xlabel('Strain', fontsize = 20)
     ax.set_xlim([0, 1])
     ax.set_ylabel('Average Aspect Ratio', fontsize = 20)
     ax.set_ylim([1, 5])
-    fit = np.polyfit(hot['bulk_strain'], hot['AR'], 1)    
-    x = np.linspace(0, 1, 20) 
-    y = fit[1] + (fit[0] * x) 
-    print(y)
+        
+    x = np.linspace(-1, 1, 20) 
+    offset = 1.324021
+    model = pandas.ols(y = hot['AR'], x = hot['bulk_strain'], intercept = False)
+    hot['fit'] = model.y_fitted    
+    hot['AR_with_offset'] = hot['AR'] - offset
+    model_offset = pandas.ols(y = hot['AR_with_offset'], x = hot['bulk_strain'], intercept = False)
+    print(model_offset)
+
+    y = 1.8728 * x + offset
+    
     ax.plot(x, y, 'k--')
-    ax.legend(['raw',' y = {0}x + {1}'.format("{:.2f}".format(fit[0]), "{:.2f}".format(fit[1]))], 2, fontsize = 14)
+    ax.legend(['raw',' y = 1.8728 x + 1.3240, Adj R-squared = 0.9489'], 2, fontsize = 14)
 
 
 def den_vs_bulk_strain(hot):
@@ -37,12 +45,7 @@ def den_vs_bulk_strain(hot):
     ax.set_ylabel('Dendrite Strain', fontsize = 20)
     ax.set_ylim([0, 1])
     ax.plot([0, 1], [0, 1], 'k:')
-    #'''Trying to see if we can use linear models to help get the correct r squared value
-   # model = lm.LinearRegression(fit_intercept = False)
-   # bulk_strain_array = np.array([hot['bulk_strain'][region] for region in hot['bulk_strain'].index])
-   # dendrite_strain_array = np.array([hot['den_strain'][region] for region in hot['den_strain'].index])
-   # model.fit(bulk_strain_array, dendrite_strain_array)
-    #ax.plot(bulk_strain_array, model.predict(bulk_strain_array), color = 'r')'''
+
     x = np.linspace(0, 1, 20)
     
     model_no_intercept = pandas.ols(y = hot['den_strain'], x = hot['bulk_strain'], intercept = False)
@@ -63,30 +66,16 @@ def den_vs_bulk_strain(hot):
     #plots Jesi's strain data on the linear regression of my data
     ax.plot([0.20, 0.31], [regFit[0] * 0.20 + regFit[1], regFit[0] * 0.31 + regFit[1]], 'w^')
     
-    ax.legend(['reference', 'strain y = 0.5751x, Adj R-squared = 0.9886', '1 ' + r'${\sigma}$','EBSD strain'], 2, fontsize = 16)
+    ax.legend(['reference', 'strain y = 0.5834x, Adj R-squared = 0.9436 ', '1 ' + r'${\sigma}$','EBSD strain'], 2, fontsize = 16)
     ax.plot(x, minY, 'k--')
     
-def lin_fit(x, y):
-    '''Fits a linear fit of the form mx+b to the data'''
-    fitfunc = lambda params, x: params[0] * x + params[1]    #create fitting function of form mx+b
-    errfunc = lambda p, x, y: fitfunc(p, x) - y              #create error function for least squares fit
-
-    init_a = 0.5                            #find initial value for a (gradient)
-    init_b = 0                              #find initial value for b (y axis intersection)
-    init_p = np.array((init_a, init_b))     #bundle initial values in initial parameters
-
-    #calculate best fitting parameters (i.e. m and b) using the error function
-    p1, success = sp.optimize.leastsq(errfunc, init_p.copy(), args = (x, y))
-    f = fitfunc(p1, x)          #create a fit with those parameters
-    return p1, f  
-    
 def main():
-    data = pandas.read_csv('bulk_den_strain_hot.csv', sep = ",", header = 0)
+    data = pandas.read_csv('bulk_den_strain.csv', sep = ",", header = 0)
     #room = pandas.read_csv('bulk_den_strain_rt.csv', sep = ",", header = 0)
 
     data['den_min'] = (((data['AR'] - data['std']) / 1.324021) ** .5) -1
     data['den_max'] = (((data['AR'] + data['std']) / 1.324021) ** .5) -1
     
-    den_vs_bulk_strain(data)
-    #ave_ar_vs_strain(hot)
+    #den_vs_bulk_strain(data)
+    ave_ar_vs_strain(data)
     
